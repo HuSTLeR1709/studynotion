0184b004fd91f18508e10b88ff49a69d7b5013c2
HuSTLeR1709 <nabhjeetkatiyar3@gmail.com>
2024-06-12 17:29:23 +0530

    bb

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 
 
-const BASE_URL = process.env.REACT_APP_BASE_URL ;
+const BASE_URL = "https://studynotion-vecl.vercel.app"
 
 export const categories = {
     CATEGORIES_API : BASE_URL + "/course/getAllCategory"
