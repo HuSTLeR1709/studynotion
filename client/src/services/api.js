931dccc2832b236057586785b2946944bf943de2
Nabhjeet Katiyar <94735685+HuSTLeR1709@users.noreply.github.com>
2024-06-17 08:32:56 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 
 
-const BASE_URL = process.env.REACT_APP_BASE_URL ;
+const BASE_URL = https://studynotion-server-2ydv.onrender.com/api/v1 ;
 
 export const categories = {
     CATEGORIES_API : BASE_URL + "/course/getAllCategory"
